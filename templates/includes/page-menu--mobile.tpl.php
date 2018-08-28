1e745fe52f94ff0dd75fed0069522214d1a5676b
Steve Gong <stevegong2016@gmail.com>
2018-08-29 09:44:22 +1000

    better class cleaning for mmenu content.

--- templates/includes/page-menu--mobile.tpl.php
+++ templates/includes/page-menu--mobile.tpl.php
@@ -1,8 +1,7 @@
 <!-- Mobile menu -->
 <nav id="menu">
   <?php if (!empty($page['mobile_nav'])): ?>
-      <?php $mobile_nav = preg_replace(array('/ class=".*?"/','/<p>(.*?)<\/p>/s','/<h2>(.*?)<\/h2>/s','/<i>(.*?)<\/i>/s','/<h3>(.*?)<\/h3>/s'), '', render($page['mobile_nav']));?>
-      <?php print $mobile_nav; ?>
+    <?php print render($page['mobile_nav']); ?>
   <?php endif; ?>
 </nav>
 
