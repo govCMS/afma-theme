9432b3506d7c0f2fb18c37123b48e4c34b052720
Nina Li <nina.li@iconagency.com.au>
2018-08-30 09:13:30 +1000

    Create footer styles

--- templates/includes/page-footer.tpl.php
+++ templates/includes/page-footer.tpl.php
@@ -4,7 +4,7 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="explore-the-site">
-                    <a href="#" title="explore the site" class="explore-the-site-link"><i class="fas fa-angle-down"></i> Explore the site <i class="fas fa-angle-down"></i></a>
+                    <a href="#" title="explore the site" class="explore-the-site-link"><i class="fas fa-angle-down"></i>  Explore the site  <i class="fas fa-angle-down"></i></a>
                 </div>
               <?php print render($page['footer']); ?>
             </div>
@@ -17,12 +17,13 @@
   <footer id="footer-menu" class="footer-menu">
     <div class="<?php print $container_class; ?>">
         <div class="col-md-6 footer-bottom-stay-connected">
-            <p>Stay connected</p>
+            Stay connected
             <div class="social-links">
-                <a href="#" title="AFMA Facebook" class="afma-facebook-redirect"><i class="fab fa-facebook-square"></i></a>
-                <a href="#" title="AFMA Youtube" class="afma-youtube-redirect"><i class="fab fa-youtube"></i></a>
+                <a href="#" title="AFMA Facebook" class="afma-facebook-redirect"><i class="fab fa-facebook-square fa-2x"></i></a>
+                <a href="#" title="AFMA Youtube" class="afma-youtube-redirect"><i class="fab fa-youtube fa-2x"></i></a>
             </div>
         </div>
+
         <div class="col-md-6 footer-bottom-menu">
           <?php print render($page['footer_menu']); ?>
         </div>
