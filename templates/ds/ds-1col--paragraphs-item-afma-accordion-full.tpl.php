a50b2cd21b6f927a79c4c86b654245a554115c42
Jason Guo <jason@xing.com.au>
2018-09-05 15:14:37 +1000

    accordion colour and open state

--- templates/ds/ds-1col--paragraphs-item-afma-accordion-full.tpl.php
+++ templates/ds/ds-1col--paragraphs-item-afma-accordion-full.tpl.php
@@ -8,20 +8,26 @@
 <?php
     $accordion_title = render($content['field_title']);
     $accordion_id = _human_to_machine($accordion_title).'-'.rand(100,999);
+    $accordion_open = '';
+    $accordion_link_class = 'collapsed';
+    if (render($content['field_accordion_open']) == 'Accordion open') {
+      $accordion_open = 'show';
+      $accordion_link_class = '';
+    }
 ?>
-
 <div class="card">
   <?php if (isset($title_suffix['contextual_links'])): ?>
     <?php print render($title_suffix['contextual_links']); ?>
   <?php endif; ?>
     <div class="card-header" role="tab" id="accordion-header-<?php print $accordion_id;?>">
         <h2 class="mb-0 h5">
-            <a class="collapsed" data-toggle="collapse" href="#accordion-<?php print $accordion_id; ?>" aria-expanded="true" aria-controls="accordion-<?php print $accordion_id; ?>">
+            <a class="<?php print $accordion_link_class; ?>" data-toggle="collapse" href="#accordion-<?php print $accordion_id; ?>" aria-expanded="true" aria-controls="accordion-<?php print $accordion_id; ?>">
                 <?php print $accordion_title; ?>
             </a>
         </h2>
     </div>
-    <div id="accordion-<?php print $accordion_id; ?>" class="collapse" role="tabpanel" aria-labelledby="accordion-header-<?php print $accordion_id;?>" data-parent="#accordion-group" style="">
+
+    <div id="accordion-<?php print $accordion_id; ?>" class="collapse <?php print $accordion_open; ?>" role="tabpanel" aria-labelledby="accordion-header-<?php print $accordion_id;?>" data-parent="#accordion-group" style="">
         <div class="card-body">
           <?php print render($content['field_text']); ?>
         </div>
