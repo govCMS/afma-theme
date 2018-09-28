4aef28bf433e245314c054b146bdda25d1b77e5b
Jason Guo <jason@xing.com.au>
2018-09-28 10:51:27 +1000

    mobile accessibility

--- templates/includes/page-header--mobile.tpl.php
+++ templates/includes/page-header--mobile.tpl.php
@@ -9,16 +9,16 @@
     <a href="/" aria-label="homepage">
         <img src="/<?php print path_to_theme(); ?>/img/logo-afma.svg" class="afma-logo" alt="Australian Fisheries Management Authority"/>
     </a>
-    <a href="#" class="search-btn" id="search-btn">
+    <a href="#" class="search-btn" id="search-btn" aria-label="open search form">
         <i class="far fa-search"></i>
     </a>
 
-    <a href="#menu" class="mob-menu">
+    <a href="#menu" class="mob-menu" aria-label="open and close menu">
         <i class="fal fa-bars"></i>
     </a>
 
     <div class="search" id="mob-search-popup">
-        <i class="fal fa-times" id="close-btn"></i>
+        <a href="#" aria-label="close search form"><i class="fal fa-times" id="close-btn"></i></a>
         <?php print $search_box_mobile; ?>
     </div>
 </div>
